4ef1c047a37357f147a670155a1d50ef8d961b58
Jesus <ItsHaysus@users.noreply.github.com>
2019-01-26 23:10:45 -0600

    Delete Config.php

--- Config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_SERVER', 'localhost:3036');
-define('DB_USERNAME', 'id8499309_jesus');
-define('DB_PASSWORD', '12qwaszx');
-define('DB_DATABASE', 'id8499309_ticketdatabase');
-$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
